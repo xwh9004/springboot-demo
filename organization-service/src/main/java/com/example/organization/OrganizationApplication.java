f5ef67e4fe19c4980a23850065a15adb45e33b42
xuwuhui <wuhui.xu@newtouch.cn>
2020-05-29 15:14:07 +0800

    organization-service 服务 并且注册到 eurekasvr中

---
+++ organization-service/src/main/java/com/example/organization/OrganizationApplication.java
@@ -0,0 +1,21 @@
+package com.example.organization;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * <p><b>Description:</b>
+ * TODO
+ * <p><b>Company:</b>
+ *
+ * @author created by Jesse Hsu at 14:10 on 2020/5/29
+ * @version V0.1
+ * @classNmae OrganizationApplication
+ */
+@SpringBootApplication
+public class OrganizationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrganizationApplication.class);
+    }
+}
