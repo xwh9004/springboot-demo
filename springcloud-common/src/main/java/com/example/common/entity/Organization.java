ae5d3e1507b1062cea62f75189a8f6839d0b0f65
xuwuhui <wuhui.xu@newtouch.cn>
2020-05-29 16:36:47 +0800

    springcloud-common 公共包

---
+++ springcloud-common/src/main/java/com/example/common/entity/Organization.java
@@ -0,0 +1,23 @@
+package com.example.common.entity;
+
+import lombok.Data;
+
+/**
+ * <p><b>Description:</b>
+ * TODO
+ * <p><b>Company:</b>
+ *
+ * @author created by Jesse Hsu at 16:06 on 2020/5/29
+ * @version V0.1
+ * @classNmae Organization
+ */
+@Data
+public class Organization {
+    private String name;
+
+    private String contactName;
+
+    private String contactEmail;
+
+    private String contactPhone;
+}
