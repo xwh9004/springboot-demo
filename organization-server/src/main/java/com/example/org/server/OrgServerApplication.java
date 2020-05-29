626810c4477741640a14733e66401cb36dca5856
Jesse Hsu <xwh9004@163.com>
2020-05-29 23:01:14 +0800

    organizationservice 的一个副本 验证robbin 轮训负载均衡

---
+++ organization-server/src/main/java/com/example/org/server/OrgServerApplication.java
@@ -0,0 +1,11 @@
+package com.example.org.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrgServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(OrgServerApplication.class);
+    }
+}
---
+++ organization-server/src/main/java/com/example/org/server/controller/OrgServerController.java
@@ -0,0 +1,29 @@
+package com.example.org.server.controller;
+
+import com.example.common.entity.Organization;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * <p><b>Description:</b>
+ * TODO
+ * <p><b>Company:</b>
+ *
+ * @author created by Jesse Hsu at 16:38 on 2020/5/29
+ * @version V0.1
+ * @classNmae OrganizationController
+ */
+@RequestMapping( "v1/organizations" )
+@RestController
+public class OrgServerController {
+
+    @RequestMapping( "/{organizationId}" )
+    public Organization getOrganization(@PathVariable( value = "organizationId" ) String organizationId) {
+        return new Organization()
+                .withName("test-org-server")
+                .withContactEmail("test@1233.com")
+                .withContactName("test")
+                .withContactPhone("1233455667");
+    }
+}
