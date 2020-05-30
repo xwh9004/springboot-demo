c8c9e4b26ef6738b79458b34bc2162d0c902630f
Jesse Hsu <xwh9004@163.com>
2020-05-30 17:37:11 +0800

    organization实体移入公共common包中

--- springcloud-common/src/main/java/com/example/common/entity/Organization.java
+++ springcloud-common/src/main/java/com/example/common/entity/Organization.java
@@ -2,23 +2,29 @@ package com.example.common.entity;
 
 import lombok.Data;
 
+import javax.persistence.*;
+import java.io.Serializable;
+
 /**
- * <p><b>Description:</b>
- * TODO
- * <p><b>Company:</b>
- *
  * @author created by Jesse Hsu at 16:06 on 2020/5/29
  * @version V0.1
  * @classNmae Organization
  */
 @Data
-public class Organization {
+@Entity
+@Table(name = "organizations")
+public class Organization implements Serializable {
+
+    @Id
+    @Column(name = "org_id",nullable = false)
+    private String organizationId;
+    @Column(name = "name",nullable = false)
     private String name;
-
+    @Column(name = "contact_name",nullable = false)
     private String contactName;
-
+    @Column(name = "contact_email",nullable = false)
     private String contactEmail;
-
+    @Column(name = "contact_phone",nullable = false)
     private String contactPhone;
 
     public Organization withName (String name){
